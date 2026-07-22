09a32c0067d845da5dc34143af78613c5fbd92ec
soportetecnicotrvely-js <soporte.tecnico.trvely@gmail.com>
2026-07-21 21:23:18 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("Portal Trvely");
+/**
+ * =====================================================
+ * PORTAL DE VINCULACIÓN TRVELY
+ * app.js v1.0
+ * =====================================================
+ */
+
+
+const formulario = document.getElementById(
+    "formulario-candidato"
+);
+
+
+formulario.addEventListener(
+    "submit",
+    function(event){
+
+        event.preventDefault();
+
+
+        alert(
+            "Formulario preparado correctamente. La conexión con Supabase se realizará en la siguiente etapa."
+        );
+
+
+    }
+);
