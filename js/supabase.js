512343130b28cb76a9f07eb53d698a79556c9581
soportetecnicotrvely-js <soporte.tecnico.trvely@gmail.com>
2026-07-22 21:28:30 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -8,7 +8,7 @@
  */
 
 const SUPABASE_URL = "https://psiiydjszabextmvjfzd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_SwlxgrWrwWAR99DuGpoOBg_Ugq9QuyM";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaWl5ZGpzemFiZXh0bXZqZnpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNTkxMzEsImV4cCI6MjA5OTYzNTEzMX0.FTOXxAqxNsf0alSwY1tA-mjhM3bb4PVHiS8ExT2ZC8o";
 
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
